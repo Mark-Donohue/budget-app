2443dac82e6b4dcb69d7f538a019047034df1fdd
Mark Donohue <markbdonohue@gmail.com>
2022-08-07 16:07:35 -0400

    Add intial register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Register</h1>
+      <input type="email" name="email" v-model="email" placeholder="Enter your email address" />
+      <br>
+      <input type="password" name="password" v-model="password" placeholder="Enter your password" />
+      <br>
+      <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+<style scoped>
+</style>
