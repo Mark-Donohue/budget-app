f6700724963b463f5669325fdc289e0c04ab6a4e
Mark Donohue <markbdonohue@gmail.com>
2022-08-07 16:08:59 -0400

    Add router changes

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
-// Initialize app
+// Initialize express server
 const app = express();
-app.use(morgan('combine'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(process.env.PORT || 8081);
+
+app.get('/status', (request, response) => {
+  response.send('OK');
+});
